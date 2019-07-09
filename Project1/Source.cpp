374f5c63445fb2a7348f39a596d4cbf2b4c627e7
Heather Kemp <heatherkemp7@yahoo.com>
2019-07-09 16:02:11 -0700

    Finish getting basic png export but still need bug fixes

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,7 +1,10 @@
 #include <cstdio>
+#define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 #include <stdio.h>
 #include <string> 
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
 using namespace std;
 
 int main() {
@@ -9,9 +12,9 @@ int main() {
 	FILE * pFile;
 	errno_t err;
 
-	const char * fileName = "myfile.txt";
+	const char * fileName = "test.pmm";
 
-	if ((err = fopen_s(&pFile, fileName, "w")) != 0) {
+	if ((err = fopen_s(&pFile, fileName, "wb")) != 0) {
 		// There was an error and the file was not opened
 		puts("whoops");
 		return 1;
@@ -21,6 +24,27 @@ int main() {
 	int nx = 200;
 	int ny = 100;
 
-	fprintf(pFile, "P3\n%d %d\n255", nx, ny);
+	fprintf(pFile, "P3\n%d %d\n255\n", nx, ny);
+
+	for (int j = ny - 1; j >= 0; j--) {
+		for (int i = 0; i < nx; i++) {
+			float r = float(i) / float(nx);
+			float g = float(j) / float(ny);
+			float b = .2f;
+			int ir = int(255.99 * r);
+			int ig = int(255.99 * g);
+			int ib = int(255.99 * b);
+			fprintf(pFile, "%d %d %d\n", ir, ig, ib);
+
+		}
+	}
+
+	int x, y, n;
+
+	stbi_write_png("imageResult.png", nx, ny, 3, pFile, 0);
+
 	fclose(pFile);
+
+	return 0;
+
 }
