74249652e90703ffcd367f28a464f2710692f862
Heather Kemp <heatherkemp7@yahoo.com>
2019-07-11 10:52:48 -0700

    Switching from writing to pmm to writing to png directly
    Still need to update file names/etc but the mvp from chapter 1 is done
    
    Co-Authored-By: Alic Szecsei <alic.szecsei@microsoft.com>

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -9,41 +9,32 @@ using namespace std;
 
 int main() {
 	
-	FILE * pFile;
-	errno_t err;
-
-	const char * fileName = "test.pmm";
-
-	if ((err = fopen_s(&pFile, fileName, "wb")) != 0) {
-		// There was an error and the file was not opened
-		puts("whoops");
-		return 1;
-	}
-
-	// File was opened, pFile can be used to access the stream.
 	int nx = 200;
 	int ny = 100;
+	int channels = 3; // RGB; no alpha
 
-	fprintf(pFile, "P3\n%d %d\n255\n", nx, ny);
+	uint8_t* data = new uint8_t[nx * ny * channels];
 
-	for (int j = ny - 1; j >= 0; j--) {
+	for (int j = 0; j < ny; j++) {
 		for (int i = 0; i < nx; i++) {
 			float r = float(i) / float(nx);
 			float g = float(j) / float(ny);
 			float b = .2f;
-			int ir = int(255.99 * r);
-			int ig = int(255.99 * g);
-			int ib = int(255.99 * b);
-			fprintf(pFile, "%d %d %d\n", ir, ig, ib);
+			int ir = int(255.00 * r);
+			int ig = int(255.00 * g);
+			int ib = int(255.00 * b);
+
+			int index = 3 * (j * nx + i);
+			data[index + 0] = ir;
+			data[index + 1] = ig;
+			data[index + 2] = ib;
 
 		}
 	}
 
-	int x, y, n;
-
-	stbi_write_png("imageResult.png", nx, ny, 3, pFile, 0);
-
-	fclose(pFile);
+	stbi_flip_vertically_on_write(1);
+	stbi_write_png("image.png", nx, ny, 3, data, 0);
+	delete[] data;
 
 	return 0;
 
