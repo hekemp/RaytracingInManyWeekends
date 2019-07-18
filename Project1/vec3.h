b6f77c649c88be44e75787c1c48921c7483c5d3b
Heather K <2heatherk@gmail.com>
2019-07-18 14:49:48 -0700

    fix float/double warning

--- Project1/vec3.h
+++ Project1/vec3.h
@@ -51,7 +51,7 @@ inline std::ostream& operator<<(std::ostream &os, const vec3 &t) {
 }
 
 inline void vec3::make_unit_vector() {
-	float k = 1.0 / sqrt((e[0] * e[0]) + (e[1] * e[1]) + (e[2] * e[2]));
+	float k = 1.0f / sqrt((e[0] * e[0]) + (e[1] * e[1]) + (e[2] * e[2]));
 	e[0] *= k;
 	e[1] *= k;
 	e[2] *= k;
@@ -132,7 +132,7 @@ inline vec3& vec3::operator/= (const vec3 &v) {
 }
 
 inline vec3& vec3::operator/=(const float t) {
-	float k = 1.0 / t;
+	float k = 1.0f / t;
 	e[0] /= k;
 	e[1] /= k;
 	e[2] /= k;
