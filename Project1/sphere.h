f71cafb47d990f2df503aa506d39adaab378e413
Heather Kemp <heatherkemp7@yahoo.com>
2019-07-19 14:37:54 -0700

    align the tmax/t_max for sanity

--- Project1/sphere.h
+++ Project1/sphere.h
@@ -7,7 +7,7 @@ class sphere : public hitable {
 public:
 	sphere() {}
 	sphere(vec3 cen, float r) : center(cen), radius(r) {};
-	virtual bool hit(const ray& r, float tmin, float tmax, hit_record& rec) const;
+	virtual bool hit(const ray& r, float t_min, float t_max, hit_record& rec) const;
 	vec3 center;
 	float radius;
 };
