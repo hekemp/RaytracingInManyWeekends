1fa1dddc06e4abc3620e00651801be510f8a990a
Heather Kemp <heatherkemp7@yahoo.com>
2019-07-31 10:45:59 -0700

    Add defocus blur
    Complete chapter 11

--- Project1/camera.h
+++ Project1/camera.h
@@ -7,22 +7,20 @@
 #define M_PI           3.14159265358979323846
 #endif
 
-#ifndef drand48()
 #define drand48() (double)rand() / RAND_MAX
-#endif
 
 vec3 random_in_unit_disk() {
 	vec3 p;
 	do {
-		p = 2.0f * vec3(drand48(), drand48(), 0) - vec3(1, 1, 0);
+		p = 2.0f * vec3(float(drand48()), float(drand48()), 0) - vec3(1, 1, 0);
 	} while (dot(p, p) >= 1.0f);
 	return p;
 }
 
 class camera {
 public: 
-	camera(vec3 lookfrom, vec3 lookat, vec3 vup, float vfov, float aspect) { // vfov is top to bottom in degrees
-		vec3 u, v, w;
+	camera(vec3 lookfrom, vec3 lookat, vec3 vup, float vfov, float aspect, float aperture, float focus_dist) { // vfov is top to bottom in degrees
+		lens_radius = aperture / 2;
 		float theta = vfov * float(M_PI) / 180;
 		float half_height = tan(theta / 2);
 		float half_width = aspect * half_height;
@@ -30,20 +28,24 @@ public:
 		w = unit_vector(lookfrom - lookat);
 		u = unit_vector(cross(vup, w));
 		v = cross(w, u);
-		lower_left_corner = origin - (half_width * u) - (half_height * v) - w;
-		horizontal = 2 * half_width * u;
-		vertical = 2 * half_height * v;
+		lower_left_corner = origin - (half_width * focus_dist * u) - (half_height * focus_dist * v) - (w * focus_dist);
+		horizontal = 2 * half_width * focus_dist * u;
+		vertical = 2 * half_height * focus_dist * v;
 		
 	}
 
 	ray get_ray(float s, float t) {
-		return ray(origin, lower_left_corner + (s * horizontal) + (t * vertical) - origin);
+		vec3 rd = lens_radius * random_in_unit_disk();
+		vec3 offset = (u * rd.x()) + (v * rd.y());
+		return ray(origin + offset, lower_left_corner + (s * horizontal) + (t * vertical) - origin - offset);
 	}
 
 	vec3 origin;
 	vec3 lower_left_corner;
 	vec3 horizontal;
 	vec3 vertical;
+	vec3 u, v, w;
+	float lens_radius;
 };
 
 #endif
